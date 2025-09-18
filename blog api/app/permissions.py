c38dafba63eac0d71735d9478c1ff799085dfc58
Yosef S <flaslnm@gmail.com>
2025-09-18 18:33:51 +0330

    add blog api solution

--- blog api/app/permissions.py
+++ blog api/app/permissions.py
@@ -2,4 +2,20 @@ from rest_framework import permissions
 
 
 class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
-    pass
+    def has_permission(self, request, view):
+        if request.method in ('GET', 'HEAD', 'OPTIONS'):
+            return True
+        else:
+            if bool(request.user and request.user.is_authenticated):
+                return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in ('GET', 'HEAD', 'OPTIONS'):
+            return True
+        elif request.user.is_superuser:
+            return True
+        else:
+            if obj.owner.id == request.user.id:
+                return True
+        return False
--- blog api/app/serializers.py
+++ blog api/app/serializers.py
@@ -1,10 +1,71 @@
-class PostSerializer:
-    pass
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+
+from .models import Comment, Post
+class PostSerializer(ModelSerializer):
+    owner = serializers.SerializerMethodField()
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'body',
+            'created',
+            'owner',
+        ]
+        read_only_fields = ['created', 'owner']
+    def get_owner(self, obj):
+        return obj.owner.username
 
 
-class PostDetailSerializer:
-    pass
+class PostDetailSerializer(ModelSerializer):
+    owner = serializers.SerializerMethodField()
+    comment_set = serializers.SerializerMethodField()
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'body',
+            'created',
+            'owner',
+            'updated',
+            'comment_set',
+        ]
+        read_only_fields = [
+            'created',
+            'updated',
+            'owner',
+            'comment_set',
+        ]
+
+    def get_owner(self, obj):
+        return obj.owner.username
+    
+    def get_comment_set(self, obj):
+        comments = obj.comment_set.all()
+        return [f"http://localhost:8000/api/comments/{i.id}/" for i in comments]
 
 
-class CommentSerializer:
-    pass
+class CommentSerializer(ModelSerializer):
+    owner = serializers.SerializerMethodField()
+    post = serializers.SerializerMethodField()
+    class Meta:
+        model = Comment
+        fields = [
+            'post',
+            'owner',
+            'body',
+            'created',
+            'updated',
+        ]
+        read_only_fields = [
+            'post',
+            'owner',
+            'created',
+            'updated',
+        ]
+        
+    def get_post(self, obj):
+        return f"http://localhost:8000/api/posts/{obj.post_id}/"
+    
+    def get_owner(self, obj):
+        return obj.owner.username
